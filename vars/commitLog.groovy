7f19dc692c13f4c84ac78c53f9c5b1424e9a7745
viennt-alxvn <55728851+viennt-alxvn@users.noreply.github.com>
2021-01-06 16:51:07 +0700

    up

--- vars/commitLog.groovy
+++ vars/commitLog.groovy
@@ -1,16 +1,35 @@
-def call(String name = 'human') {
-  echo "Hello, ${name}."
-}
+def call() {
+  def env
+  def currentBuild
 
-def welcome(String name = 'human') {
-  echo "Welcome  ${name}!"
-
-}
-
-def info(String name = 'human') {
-    def per = new utils.DemoClass()
-    per.name = name
-    per.age = 20
-     per.increaseAge(10)
-    echo "Info: Name: ${per.name} - Age: ${per.age}"
+  echo "env ${env}"
+   echo "c ${currentBuild}"
+  def log = ""
+        def changeLogSets = currentBuild.changeSets
+        for (int i = 0; i < changeLogSets.size(); i++) {
+            def entries = changeLogSets[i].items
+            for (int j = 0; j < entries.length; j++) {
+                def entry = entries[j]
+                log += "<b>Branch:</b> " + env.BRANCH_NAME
+                log += "<br>"
+                log += "<b>Commit:</b> ${entry.commitId} "
+                log += "<br>"
+                log += "<b>Author:</b> ${entry.author} "
+                log += "<br>"
+                log += "<b>Date:</b> ${new Date(entry.timestamp)} "
+                log += "<br>"
+                log += "<b>Message:</b> ${entry.msg} "
+                log += "<br>"
+                log +="<b>Changed Paths:</b>"
+                log += "<br>"
+                def files = new ArrayList(entry.affectedFiles)
+                for (int k = 0; k < files.size(); k++) {
+                    def file = files[k]
+                    listFile += "${file.path},"
+                    log += "<b> &nbsp;&nbsp; ${file.editType.name} -- ${file.path} </b><br>"
+                }
+                log+="---------------------------<br>"
+            }
+        }
+    return log;
 }
