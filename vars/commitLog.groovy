79c07d490ae9d02720278dd3e5b36c4ee38f154e
viennt-alxvn <55728851+viennt-alxvn@users.noreply.github.com>
2021-01-06 17:43:58 +0700

    update

--- vars/commitLog.groovy
+++ vars/commitLog.groovy
@@ -1,36 +1,34 @@
 def info(script) {
-  def env
-  // def currentBuild
 
   println "env ${script.env.BRANCH_NAME}"
   
-  // def log = ""
-  //       def changeLogSets = script.currentBuild.changeSets
-  //        echo "c ${changeLogSets.size()}"
-  //       for (int i = 0; i < changeLogSets.size(); i++) {
-  //           def entries = changeLogSets[i].items
-  //           for (int j = 0; j < entries.length; j++) {
-  //               def entry = entries[j]
-  //               log += "<b>Branch:</b> " + env.BRANCH_NAME
-  //               log += "<br>"
-  //               log += "<b>Commit:</b> ${entry.commitId} "
-  //               log += "<br>"
-  //               log += "<b>Author:</b> ${entry.author} "
-  //               log += "<br>"
-  //               log += "<b>Date:</b> ${new Date(entry.timestamp)} "
-  //               log += "<br>"
-  //               log += "<b>Message:</b> ${entry.msg} "
-  //               log += "<br>"
-  //               log +="<b>Changed Paths:</b>"
-  //               log += "<br>"
-  //               def files = new ArrayList(entry.affectedFiles)
-  //               for (int k = 0; k < files.size(); k++) {
-  //                   def file = files[k]
-  //                   listFile += "${file.path},"
-  //                   log += "<b> &nbsp;&nbsp; ${file.editType.name} -- ${file.path} </b><br>"
-  //               }
-  //               log+="---------------------------<br>"
-  //           }
-  //       }
-  //   return log;
+  def log = ""
+        def changeLogSets = script.currentBuild.changeSets
+         echo "c ${changeLogSets.size()}"
+        for (int i = 0; i < changeLogSets.size(); i++) {
+            def entries = changeLogSets[i].items
+            for (int j = 0; j < entries.length; j++) {
+                def entry = entries[j]
+                log += "<b>Branch:</b> " + env.BRANCH_NAME
+                log += "<br>"
+                log += "<b>Commit:</b> ${entry.commitId} "
+                log += "<br>"
+                log += "<b>Author:</b> ${entry.author} "
+                log += "<br>"
+                log += "<b>Date:</b> ${new Date(entry.timestamp)} "
+                log += "<br>"
+                log += "<b>Message:</b> ${entry.msg} "
+                log += "<br>"
+                log +="<b>Changed Paths:</b>"
+                log += "<br>"
+                def files = new ArrayList(entry.affectedFiles)
+                for (int k = 0; k < files.size(); k++) {
+                    def file = files[k]
+                    listFile += "${file.path},"
+                    log += "<b> &nbsp;&nbsp; ${file.editType.name} -- ${file.path} </b><br>"
+                }
+                log+="---------------------------<br>"
+            }
+        }
+    return log;
 }
