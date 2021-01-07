ab12d27b52f81c75b7690463d2822f3ea3c6f33b
viennt-alxvn <55728851+viennt-alxvn@users.noreply.github.com>
2021-01-07 10:13:59 +0700

    up

--- vars/fileDiff.groovy
+++ vars/fileDiff.groovy
@@ -4,39 +4,40 @@ def info(String text, String listFile, String buildUrl){
     def indexFile = 0
     def arrFile = listFile.split(',')
   
-    bodyHtml += """<p>Check build changes on Jenkins <b><a href="${buildUrl}/last-changes">here</a></b>.</p><br>
+    if (arrFile.count() > 0) {
+        bodyHtml += """<p>Check build changes on Jenkins <b><a href="${buildUrl}/last-changes">here</a></b>.</p><br>
                         <p><b>Files Diff</b></p>"""
 
-    text.split("\n").each {
-        param ->
-        if (param.startsWith('Index:')) {   
-            if (count >0) {
-                bodyHtml += "<br>"
-            }
-            count = 0;                        
-            bodyHtml +="<span><b>" + param+ "</b></span><br>"   
-            
-            // call php file to check syntax
-            def test =  callSh(arrFile[indexFile])
-            //result check
-            bodyHtml += "&nbsp;&nbsp; <b> * Check Syntax: ${test}</b><br>"     
+        text.split("\n").each {
+            param ->
+            if (param.startsWith('Index:')) {   
+                if (count >0) {
+                    bodyHtml += "<br>"
+                }
+                count = 0;                        
+                bodyHtml +="<span><b>" + param+ "</b></span><br>"   
+                
+                // call php file to check syntax
+                def test =  callSh(arrFile[indexFile])
+                //result check
+                bodyHtml += "&nbsp;&nbsp; <b> * Check Syntax: ${test}</b><br>"     
 
-            indexFile++                
-        } else if (count <= 4){
-            bodyHtml +="<span>" + param+ "</span><br>"
-        } else {
-            if (param.startsWith('-')){
-                bodyHtml +="<code style='background-color:#fdaeb7; white-space:pre-wrap'>" + param.replace("<", "&lt;").replace(">","&gt;") +"</code>"
-            } else if(param.startsWith('+')) {
-                bodyHtml +="<code style='background-color:#bef5cb; white-space:pre-wrap'>" + param.replace("<", "&lt;").replace(">","&gt;") +"</code>"
+                indexFile++                
+            } else if (count <= 4){
+                bodyHtml +="<span>" + param+ "</span><br>"
             } else {
-                bodyHtml +="<code style='white-space:pre'>" + param.replace("<", "&lt;").replace(">","&gt;") + "</code>"
-            }
+                if (param.startsWith('-')){
+                    bodyHtml +="<code style='background-color:#fdaeb7; white-space:pre-wrap'>" + param.replace("<", "&lt;").replace(">","&gt;") +"</code>"
+                } else if(param.startsWith('+')) {
+                    bodyHtml +="<code style='background-color:#bef5cb; white-space:pre-wrap'>" + param.replace("<", "&lt;").replace(">","&gt;") +"</code>"
+                } else {
+                    bodyHtml +="<code style='white-space:pre'>" + param.replace("<", "&lt;").replace(">","&gt;") + "</code>"
+                }
+            }            
+            count++            
         }
-        
-        count++
-        
     }
+    
     return bodyHtml
 }
 
